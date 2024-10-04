5dd2b3a1d29802c8a5795cf380a97a833c65b929
Christian Peña <ch.penaf@gmail.com>
2024-10-04 15:13:33 -0300

    Adding colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  { "ellisonleao/gruvbox.nvim" },
+
+  { "navarasu/onedark.nvim" },
+
+  { "sainnhe/edge" },
+
+  { "sainnhe/sonokai" },
+
+  { "polirritmico/monokai-nightasty.nvim" },
+
+  { "NTBBloodbath/doom-one.nvim" },
+
+  { "bluz71/vim-moonfly-colors" },
+
+  { "p00f/alabaster.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai-nightasty",
+      --colorscheme = "moonfly",
+    },
+  },
+}
